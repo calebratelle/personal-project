5886c8dc9aa1ce007b28a2d404ac06971bdc4b5f
Caleb Ratelle <caleb.ratelle@gmail.com>
2023-09-27 16:27:08 -0600

    goals accomplished for the day.

--- src/Elements/JournalEntry.jsx
+++ src/Elements/JournalEntry.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
 
@@ -7,8 +7,10 @@ import axios from "axios";
 //update Journal.jsx so that journal entries are presented with their respective prompts.
 
 const JournalEntry = ({ prompt, getCourseContent }) => {
-  const [userResponses, setUserResponses] = useState(prompt.journals[0] ? prompt.journals[0].journalEntry : '');
-  const userId = useSelector(state => state.userId)
+  const [userResponses, setUserResponses] = useState(
+    prompt.journals[0] ? prompt.journals[0].journalEntry : ""
+  );
+  const userId = useSelector((state) => state.userId);
 
   const handleResponseChange = (e) => {
     setUserResponses(e.target.value);
@@ -16,12 +18,17 @@ const JournalEntry = ({ prompt, getCourseContent }) => {
 
   const handleSubmit = () => {
     const promptId = prompt.promptId;
-
-
-    axios.post("/api/postjournal", { userId, promptId, journalEntry: userResponses }).then((response) => {
-      console.log(response.status, response.data.token);
-    getCourseContent()});
-
+    axios
+      .post("/api/postjournal", {
+        userId,
+        promptId,
+        journalEntry: userResponses,
+      })
+      .then((response) => {
+        console.log(response.status, response.data.token);
+        window.reload(true);
+        getCourseContent();
+      });
   };
 
   return (
